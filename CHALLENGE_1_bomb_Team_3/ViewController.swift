2eb220cf8e4f11bbef73d53634485487c8d6a5a8
MAKSIM89PW <123>
2023-08-08 01:48:03 +0300

    start

---
+++ CHALLENGE_1_bomb_Team_3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CHALLENGE_1_bomb_Team_3
+//
+//  Created by Максим Нурутдинов on 08.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
